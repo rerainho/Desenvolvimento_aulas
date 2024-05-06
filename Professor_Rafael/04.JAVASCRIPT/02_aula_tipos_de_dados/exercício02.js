0bae46b9c9626db21492f1ae3a984800fbfd7998
rerainho <renata.rainho95@gmail.com>
2024-05-06 19:07:58 -0300

    java exercício dois e continuidade do exercício três

---
+++ Professor_Rafael/04.JAVASCRIPT/02_aula_tipos_de_dados/exercício02.js
@@ -0,0 +1,14 @@
+
+
+console.log();
+const nome_pessoa = 'Renata';
+const altura = 1.51;
+const peso = 49;
+const x = 49 / 1.51 * 2;
+const y = undefined;
+let z;
+
+const hello = `Meu nome é ${nome_pessoa} e eu possuo a altura ${altura} e meu peso ${peso} e meu IMC ${x}`;
+console.log(hello);
+
+
