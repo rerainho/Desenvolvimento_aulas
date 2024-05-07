45036cbc2315b9bc9123fa21a4d50a2bca6ab3b7
rerainho <renata.rainho95@gmail.com>
2024-05-06 21:06:26 -0300

    exercício estrutura

--- Professora_Camila/04.JAVASCRIPT/01_aula_estrutura_e_sintaxe/indexExemplo.js
+++ Professora_Camila/04.JAVASCRIPT/01_aula_estrutura_e_sintaxe/indexExemplo.js
@@ -60,3 +60,20 @@ else{
     console.log('Resultado da condição: ${peça===sapato}');
     console.log('peça não é um vestido, portanto é um calçado');
 }
+
+//*Estrutura em multiplas condições if e else (se dentro de se)
+console.clear();
+const f = 4;
+const g = 11; 
+if(f > 5 || g > 10){
+    console.log('Condição é ${f>5 || g>10}');
+    console.log('f é maior que 5 ou g é maior que 10');
+}
+else 
+    console.log('f não é maior que 5 ou g é maior que 10');
+
+if (f > 5 && g >10) console.log('f é maior que 5 e g é maior que 10');
+else{
+    console.log('condição é ${f > 5 && g > 10}');
+    console.log('f não é maoir que 5 ou g não é maoir que 10')
+}
