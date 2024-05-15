9adddba19633ce2e4185f6b4a34a8d1bd0810130
Renata Rainho <164105631+rerainho@users.noreply.github.com>
2024-05-15 17:40:56 -0300

    Add files via upload

---
+++ Professor_Rafael/04.JAVASCRIPT/04_laços_repetição/exer.js
@@ -0,0 +1,19 @@
+let contador = 0;
+while (contador <= 49) {
+    contador++;
+console.log(`Valor = ${contador}`);
+}
+
+console.clear();
+let cont = 10;
+while (cont >= 0) {
+console.log(`Valor = ${cont}`);
+cont--;
+}
+
+console.clear();
+for (let i = 1; i <= 100; i++) {
+    if (i % 2 === 0) {
+        console.log("Número par:", i);
+    }
+};
