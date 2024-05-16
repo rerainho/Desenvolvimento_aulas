1ef3e6d631e674146195f7b81b9433a19cbddeb6
rerainho <renata.rainho95@gmail.com>
2024-05-15 21:07:10 -0300

    classes e funções

---
+++ Professora_Camila/04.JAVASCRIPT/04_aula_objetos/classesEfunções.js
@@ -0,0 +1,22 @@
+// sintaxe de classe// costructior é um método utilizado 
+class nomeClass{
+    constructor(){exemplos}
+}
+//usando propriedades, atributos, estáfo da classe de herança para essa variáveis// Declaração:
+//Classe:
+class Carro{
+    constructor(name, year){
+        this.name=name;
+        this.year=year;
+    }
+}
+//Instanciando (criando) objetos à classe
+let meuCarro1 = new Carro ('Ford', 2014);
+let meuCarro2 = new Carro ('Audi', 2019);
+
+// Imprimindo valores
+console.log(meuCarro1.name);
+console.log(meuCarro1.year);
+
+console.log(meuCarro2.name);
+console.log(meuCarro2.year);
