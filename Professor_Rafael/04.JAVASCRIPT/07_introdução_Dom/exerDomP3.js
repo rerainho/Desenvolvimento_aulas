dfe23120e78f5e887c35f0aec277f88a4e40f500
rerainho <renata.rainho95@gmail.com>
2024-05-28 21:19:13 -0300

    atualizações da aula 7

--- Professor_Rafael/04.JAVASCRIPT/07_introdução_Dom/exerDomP3.js
+++ Professor_Rafael/04.JAVASCRIPT/07_introdução_Dom/exerDomP3.js
@@ -3,7 +3,8 @@ let paragrafo = document.querySelector('p');
 paragrafo.style.background = '#FBBEBA';
 
 
-let linhas = document.querySelectorAll('section');
+let linhas = document.querySelectorAll('my-teste');
+
 for (i = 0; i < section.length; i++) {
 linhas[i].style.border = '1px solid #0000FF';
 linhas[i].style.padding = '3px';
