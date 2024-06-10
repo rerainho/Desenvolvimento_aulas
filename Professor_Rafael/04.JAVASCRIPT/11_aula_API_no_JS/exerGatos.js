3906629f8d19e698320e86ed65fa92c09b857c66
rerainho <renata.rainho95@gmail.com>
2024-06-10 20:59:21 -0300

    api de CEP exercício javascript

--- Professor_Rafael/04.JAVASCRIPT/11_aula_API_no_JS/exerGatos.js
+++ Professor_Rafael/04.JAVASCRIPT/11_aula_API_no_JS/exerGatos.js
@@ -12,18 +12,9 @@ function getImage(e) {
     })
     .then((data) => {
     console.log(data);
-    document.getElementById('id').innerText = data[0,1,2,3,4].id;
-    let imagem = `<img class="rounded img-fluid" src="${data[0,1,2,3,4].url}" />`;
+    document.getElementById('id').innerText = data[0].id;
+    let imagem = `<img class="rounded img-fluid" src="${data[0].url}" />`;
     // Insere a imagem no elemento HTML com id imagem_aqui
-    document.querySelector('#imagem_aqui_1').innerHTML = imagem;
-    document.querySelector('#imagem_aqui_2').innerHTML = imagem;
-    document.querySelector('#imagem_aqui_3').innerHTML = imagem;
-    document.querySelector('#imagem_aqui_4').innerHTML = imagem;
-    document.querySelector('#imagem_aqui_5').innerHTML = imagem;
-    document.querySelector('#imagem_aqui_6').innerHTML = imagem;
-    document.querySelector('#imagem_aqui_7').innerHTML = imagem;
-    document.querySelector('#imagem_aqui_8').innerHTML = imagem;
-    document.querySelector('#imagem_aqui_9').innerHTML = imagem;
-    document.querySelector('#imagem_aqui_10').innerHTML = imagem;
+    document.querySelector('#imagem_aqui').innerHTML = imagem;
     });
     }
